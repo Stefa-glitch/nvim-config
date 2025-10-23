4ac4bce7c1fbf21ef2630b8f2793ba8216db07e8
Stefa-glitch <stefanribarac@zmajsm.edu.rs>
2025-10-23 19:21:02 +0200

    Short cut changed

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -6,17 +6,7 @@ return {
     config = function()
       require("nvim-treesitter.configs").setup({
         ensure_installed = {
-          "lua",
-          "javascript",
-          "typescript",
-          "python",
-          "html",
-          "css",
-          "json",
-          "markdown",
-          "markdown_inline",
-          "rust",
-        },
+          },
         highlight = { enable = true },
         indent = { enable = true },
         auto_install = true,
